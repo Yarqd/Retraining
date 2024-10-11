d60553874322a6d4c56b049a289d6a8a913ca616
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-10-11 17:14:56 +0300

    6 lessons

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,10 +1,5 @@
-import modules.OneJavaBasics.ArithmeticOperations;
-import modules.OneJavaBasics.FirstProgram;
-import modules.OneJavaBasics.Introduction;
-import modules.OneJavaBasics.Statements;
-
 public class App {
     public static void main(String[] args) {
-        ArithmeticOperations.arithmeticOperations();
+        modules.OneJavaBasics.Variables.variables();
     }
 }
--- app/src/main/java/modules/OneJavaBasics/Strings.java
+++ app/src/main/java/modules/OneJavaBasics/Strings.java
@@ -1,4 +1,15 @@
 package modules.OneJavaBasics;
 
 public class Strings {
+    public static void strings() {
+        System.out.println("строкой мы считаем все, что находится внутри кавычек, даже если это пробел, один символ или "
+                + "вообще отсутствие символов.");
+        System.out.println("Dragon's mother");
+        System.out.println("Dragon's mother said \"No\"");
+        System.out.println("Для вывода таких строк используют символ экранирования: \\");
+        System.out.println("System.out.println(\"Dragon's mother said \\\"No\\\"\");");
+        System.out.println("Cделать перевод строки - можно используя символ перевода строки: \\n");
+        System.out.println("System.out.println() - тоже переводит строку");
+        System.out.println("Конкатенация - операция соединения строк. " + "<- вот этот знак (смотри в коде)");
+    }
 }
